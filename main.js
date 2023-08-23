616fa0ebf52a5976ae38b8aaa2c0f26be3afe2d8
Canario Alan <alan.union.1998@gmail.com>
2023-08-23 00:10:35 -0300

    cositas

--- main.js
+++ main.js
@@ -18,3 +18,23 @@ output.textContent = price.value;
 price.addEventListener("input", function () {
   output.textContent = price.value + "Mts";
 });
+
+// map
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoic2hlZm9yIiwiYSI6ImNsbGppdzMzejE4NHczbXVsbHFpbGUzbWoifQ.IKfPsNOSuvReSFWZ87_RDg";
+const mapa = new mapboxgl.Map({
+  container: "contend-mapa",
+  style: "mapbox://styles/mapbox/outdoors-v11",
+  center: [-60.7092999, -31.6427484],
+  zoom: 14,
+});
+const marcador = new mapboxgl.Marker({
+  color: "green",
+})
+  .setLngLat([-60.710715, -31.642394])
+  .addTo(mapa);
+const popup = new mapboxgl.Popup({}).setText("hello");
+const marcador2 = new mapboxgl.Marker({})
+  .setLngLat([-60.710099, -31.6400568])
+  .setPopup(popup)
+  .addTo(mapa);
